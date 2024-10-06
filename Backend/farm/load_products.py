b6790c5ea2168f727f0151f566d44759b3a37605
jaaaypatel1 <jaylupatel13@gmail.com>
2024-10-06 18:18:26 -0400

    Small fixes got osome more of the server to work

--- Backend/farm/load_products.py
+++ Backend/farm/load_products.py
@@ -1,28 +1,18 @@
+import pandas as pd
 import json
-from django.core.management.base import BaseCommand
-from farm.models import Product
 
-class Command(BaseCommand):
-    help = 'Load products from a JSON file'
+with open("products.json") as f:
+    data = json.load(f)
 
-    def add_arguments(self, parser):
-        parser.add_argument('json_file', type=str, help='The path to the JSON file to load')
 
-    def handle(self, *args, **kwargs):
-        json_file = kwargs['json_file']
+df = pd.DataFrame(data)
+df.head()
+df.columns
 
-        with open(json_file, 'r') as file:
-            products_data = json.load(file)
+### SQL Connection
 
-            for item in products_data:
-                product = Product(
-                    name=item['name'],
-                    description=item['description'],
-                    price=item['price'],
-                    quantity=item['quantity'],
-                    zip=item['zip'],
-                    dateHarvested=item['dateHarvested'],
-                    businessName=item['businessName']
-                )
-                product.save()
-                self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.name}'))
+import sqlite3
+
+conn = sqlite3.connect("db.sqlite3")
+c = conn.cursor()
+
